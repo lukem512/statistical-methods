6a6b63539a45d368d8af17e41aab3927325d07de
lukem512 <lukem95@gmail.com>
2016-04-14 14:41:27 +0100

    Added mode, median, range, min, max; Added tests

--- src/statistical-methods.js
+++ src/statistical-methods.js
@@ -4,58 +4,160 @@
 
 // Extract a numeric value from an array
 function num(elem) {
-	var i = parseInt(arr[i]);
-	if (i) return i;
-
-	var f = parseFloat(arr[i]);
+	var f = parseFloat(elem);
 	if (f) return f;
-
 	return 0;
 };
 
+// Sort a numeric array
+function sort(arr) {
+	return arr.sort(function(a, b) {
+		var x = num(a), y = num(b);
+		return x - y;
+	});
+};
+
+// Return the minimum value in a numeric array
+var min = module.exports.min = function(arr) {
+	if (arr.length == 0) return null;
+
+	var min = Infinity;
+	for (var i = 0; i < arr.length; i++) {
+		var elem = num(arr[i]);
+		if (elem < min) min = elem;
+	}
+
+	return min;
+};
+
+// Return the maximum value in a numeric array
+var max = module.exports.max = function(arr) {
+	if (arr.length == 0) return null;
+
+	var max = -Infinity;
+	for (var i = 0; i < arr.length; i++) {
+		var elem = num(arr[i]);
+		if (elem > max) max = elem;
+	}
+
+	return max;
+};
+
 // Compute the mean of a numeric array
-function mean(arr) {
+var mean = module.exports.mean = function(arr) {
+	if (arr.length == 0) return null;
+
 	return mu = sum(arr) / arr.length;
 };
 
-function mode(arr) {
-	// TODO
+// Compute the mode (most frequent value) of a numeric array
+// https://stackoverflow.com/posts/3451640
+var mode = module.exports.mode = function(arr) {
+	if (arr.length == 0) return null;
+
+	var sorted = sort(arr),
+    	modeMap = {},
+        maxCount = 2,
+        modes = [];
+
+    for(var i = 0; i < sorted.length; i++)
+    {
+        var el = sorted[i];
+
+        if (!modeMap[el]) modeMap[el] = 1;
+        else modeMap[el]++;
+
+        if (modeMap[el] > maxCount)
+        {
+            modes = [el];
+            maxCount = modeMap[el];
+        }
+        else if (modeMap[el] == maxCount)
+        {
+            modes.push(el);
+            maxCount = modeMap[el];
+        }
+    }
+
+    if (modes.length == 1) return modes[0];
+    else if (modes.length == 0) return null;
+    else return modes;
 };
 
-function median(arr) {
-	// TODO
+// Compute the median of a numeric array
+var median = module.exports.median = function(arr) {
+	if (arr.length == 0)
+        return null;
+
+	var sorted = sort(arr);
+	var mid = Math.floor(sorted.length / 2);
+
+	// Odd numbers
+	if (sorted.length % 2 == 1) {
+		return num(sorted[mid]);
+	}
+
+	// Even numbers
+	var top = num(sorted[mid]),
+		bot = num(sorted[mid-1]);
+	return (top + bot) / 2.0;
+};
+
+// Compute the range of a numeric array
+var range = module.exports.range = function(arr) {
+	if (arr.length == 0) return null;
+
+	var min = Infinity, max = -Infinity;
+	for (var i = 0; i < arr.length; i++) {
+		var elem = num(arr[i]);
+		if (elem < min) min = elem;
+		if (elem > max) max = elem;
+	}
+
+	return max - min;
 };
 
 // Compute the variance of a numeric array
-function variance(arr, sample) {
-	var sample = sample | false;
+// This defaults to the sample variance
+var variance = module.exports.variance = function(arr, sample) {
+	if (arr.length == 0) return null;
+
+	if (!sample) sample = true;
 	var mu = mean(arr);
 	var divisor = (sample ? arr.length - 1 : arr.length);
+
 	return (1/divisor) * sum(arr.map(function(i) {
 		return (i - mu) * (i - mu);
 	}));
 };
 
 // Compute the standard deviation of a numeric array
-function stddev(arr, sample) {
+var stddev = module.exports.stddev = function(arr, sample) {
+	if (arr.length == 0) return null;
 	var s2 = variance(arr, sample);
 	return Math.sqrt(s2);
 };
 
 // Sum all numeric elements in an array
-function sum(arr) {
+var sum = module.exports.sum = function(arr) {
+	if (arr.length == 0) return null;
+
 	var total = 0;
 	for (var i = 0; i < arr.length; i++) {
 		total += num(arr[i]);
 	}
+
 	return total;
 };
 
 // Find the product of all numeric elements in an array
-function prod(arr) {
-	var total = 0;
+var prod = module.exports.prod = function(arr) {
+	if (arr.length == 0) return null;
+
+	var total = 1;
 	for (var i = 0; i < arr.length; i++) {
 		total *= num(arr[i]);
 	}
+
 	return total;
 };
---
+++ test/test.js
@@ -0,0 +1,100 @@
+// Statistical Methods
+// Luke Mitchell, April 2016
+// https://github.com/lukem512/statistical-methods
+
+var sm = require('../src/statistical-methods');
+
+var tests = [
+	{
+		name: 'Test #1',
+		arr: [13, 18, 13, 14, 13, 16, 14, 21, 13],
+		expected: {
+			min: 13,
+			max: 21,
+			range: 8,
+			mean: 15,
+			median: 14,
+			mode: 13,
+			variance: 8,
+			stddev: 2.8284271247461903,
+			sum: 135,
+			prod: 33856437888
+		}
+	},
+
+	{
+		name: 'Test #2',
+		arr: [1, 2, 4, 7],
+		expected: {
+			min: 1,
+			max: 7,
+			range: 6,
+			mean: 3.5,
+			median: 3,
+			mode: null,
+			variance: 7,
+			stddev: 2.6457513110645907,
+			sum: 14,
+			prod: 56
+		}
+	},
+
+	{
+		name: 'Test #3',
+		arr: [ 8, 9, 10, 10, 10, 11, 11, 11, 12, 13],
+		expected: {
+			min: 8,
+			max: 13,
+			range: 5,
+			mean: 10.5,
+			median: 10.5,
+			mode: [10, 11],
+			variance: 2.0555555555555554,
+			stddev: 1.4337208778404378,
+			sum: 105,
+			prod: 14949792000
+		}
+	},
+
+	{
+		name: 'Empty array test',
+		arr: [],
+		expected: {
+			min: null,
+			max: null,
+			range: null,
+			mean: null,
+			median: null,
+			mode: null
+		}
+	}
+];
+
+
+var passed = true;
+
+tests.forEach(function(t) {
+	Object.keys(t.expected).forEach(function(o) {
+		try {
+			var value = sm[o](t.arr);
+			if (Array.isArray(value)) {
+				for (var i = 0; i < value.length; i++) {
+					if (value[i] !== t.expected[o][i]) throw Error();
+				};
+			}
+			else if (value !== t.expected[o])
+				throw Error('Unexpected result for ' + o + 
+					' (expected ' + JSON.stringify(t.expected[o]) + 
+					' and got ' + JSON.stringify(value) + ')');
+		}
+		catch (err) {
+			console.error(t.name, err);
+			passed = false;
+		}
+	});
+});
+
+if (passed) console.log('Tests succeeded');
+else console.log('Tests failed');
+
+return passed;
